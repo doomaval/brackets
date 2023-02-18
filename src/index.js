ec264446fd81de2844579e7b4628d68dae32e7c9
Valeriy Duma <doomaval@gmail.com>
2023-02-18 21:55:49 +0300

    kjgfkj

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = str.split("");
+  let stack = [];
+  let beginStr = "";
+  let endStr = "";
+  for (let item of bracketsConfig) {
+    beginStr += item[0];
+    endStr += item[1];
+  }
+  for (let item of arr) {
+    if (beginStr.includes(item)) {
+      stack.push(item);
+      break;
+    }
+    if (endStr.includes(item)) {
+      if (endStr.indexOf(item) === beginStr.indexOf(stack.at(-1))) {
+        stack.pop;
+        break;
+      }
+      return false;
+    }
+  }
+  return stack.length ? false : true;
+};
